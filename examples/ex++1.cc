8b75fccf3796cb6fb28e1ed18d1ceac0d38929e2
matthias <uso.cosmo.ray@gmail.com>
2020-09-26 15:22:02 +0200

    add example

---
+++ examples/ex++1.cc
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+class Forme {
+public:
+	virtual const char *c_str() = 0;
+	virtual ~Forme(){}
+};
+
+class Head : public Forme {
+public:
+	const char *c_str() {
+		return  " ___\n"
+			"/   \\\n"
+			"|O O|\n"
+			"| V |\n"
+			"\\ - /\n"
+			" --";
+	}
+};
+
+class Cat : public Forme {
+public:
+	const char *c_str() {
+		return  "=<oo>=     />\n"
+			"    \{====} \n"
+			"    /     \\";
+	}
+};
+
+int main()
+{
+	Forme *h = new Head;
+	Cat c;
+
+	printf("%s\n\n\n%s\n", h->c_str(), c.c_str());
+
+	delete h;
+}
